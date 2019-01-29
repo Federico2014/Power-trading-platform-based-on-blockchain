98da6e4a16229a4a2b628ebfd10dd80433bba6e3
Jachinli <jachinli12@gmail.com>
2019-01-29 23:22:49 +0800

    remove two .sol

--- contracts/HelloWorld.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.4;
-
-contract HelloWorld {
-  uint []T = [1,2,3,4];
-  function print() constant returns (string,uint[]) {
-    string memory str = "HelloWorld";
-    return (str,T);
-  }
-}
